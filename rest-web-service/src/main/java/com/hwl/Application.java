6259ed02d4edeebf97d31662fd5a808597713bf4
huwenl <huwenliang1224@163.com>
2016-08-12 01:06:59 +0800

    rest-web-service

---
+++ rest-web-service/src/main/java/com/hwl/Application.java
@@ -0,0 +1,15 @@
+package com.hwl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by huwenl on 16/8/12.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ rest-web-service/src/main/java/com/hwl/bean/Greeting.java
@@ -0,0 +1,22 @@
+package com.hwl.bean;
+
+/**
+ * Created by huwenl on 16/8/12.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
